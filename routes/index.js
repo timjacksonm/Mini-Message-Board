50837b6504c2f2f57aa2c4efc8a20bb465a252b9
Tim Jackson <timjacksonm@gmail.com>
2021-11-11 21:34:37 -0600

    added a form to submit a new message to board

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,29 @@ const messages = [
   {
     text: 'Hi there!',
     user: 'Amando',
-    added: new Date(),
+    date: new Date(),
   },
   {
     text: 'Hello World!',
     user: 'Charles',
-    added: new Date(),
+    date: new Date(),
   },
 ];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+/* GET new form. */
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Mini Messageboard', messages: messages });
+});
+
+/* Post new submission */
+router.post('/new', function (req, res, next) {
+  const { name, message } = req.body;
+  messages.push({ user: name, text: message, date: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
